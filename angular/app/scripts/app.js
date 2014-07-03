95617f82c4e156832b82a0e520ad270fb13ed488
Todd Pinkerton <tpinkerton@paypal.com>
2014-07-02 22:56:44 -0400

    skeleton rails app and yeoman angular

---
+++ angular/app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
